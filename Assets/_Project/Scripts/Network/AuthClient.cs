736105dfc41bf2ce9125f15c5f23f6527e527319
Jangmyun <cndlsrb2739@gmail.com>
2026-01-19 21:32:21 +0900

    [Feat]: Google Login 함수 작성

--- Assets/_Project/Scripts/Network/AuthClient.cs
+++ Assets/_Project/Scripts/Network/AuthClient.cs
@@ -12,6 +12,18 @@ public class AuthClient : MonoBehaviour
         Debug.Log("[AuthClient] Config Loaded");
     }
 
+    public void LoginWithGoogle()
+    {
+        if (config == null)
+        {
+            Debug.LogError("AppConfig is not initialized in AuthClient!");
+            return;
+        }
 
+        string loginUrl = $"{config.apiBaseUrl}/auth/login/google";
+
+        Debug.Log($"[Auth] Opening Login URL: {loginUrl}");
+        Application.OpenURL(loginUrl);
+    }
 
 }
