72b705b674efa2cad74e7f67a05aa44c3a6840d2
Jangmyun <cndlsrb2739@gmail.com>
2026-02-18 10:29:52 +0900

    [Chore]: AuthManager 선언 위치 이동

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -7,15 +7,15 @@ public class Managers : MonoBehaviour
 
     #region [Sub Managers]
 
+    AuthManager _auth = new AuthManager();
     ResourceManager _resource = new ResourceManager();
     UIManager _ui = new UIManager();
 
+
+    public static AuthManager Auth { get { return Instance._auth; } }
     public static ResourceManager Resource { get { return Instance._resource; } }
     public static UIManager UI { get { return Instance._ui; } }
 
-
-    AuthManager _auth = new AuthManager();
-    public static AuthManager Auth { get { return Instance._auth; } }
 
     #endregion
 
