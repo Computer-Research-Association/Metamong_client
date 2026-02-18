75a1b9148c597a02336a679d027daee7c19d0bf8
Jangmyun <cndlsrb2739@gmail.com>
2026-02-18 14:28:27 +0900

    [Feat]: UserManager - UserData CRUD 구현

--- Assets/Scripts/Managers/UserManager.cs
+++ Assets/Scripts/Managers/UserManager.cs
@@ -1,5 +1,43 @@
+using System;
+using Metamong.Core;
 using UnityEngine;
 
 public class UserManager
 {
+    private UserData _currentUser;
+
+    public UserData CurrentUser
+    {
+        get => _currentUser;
+        private set
+        {
+            _currentUser = value;
+            OnUserDataChanged?.Invoke(value);
+        }
+    }
+
+    public bool HasUser => _currentUser != null;
+
+    public event Action<UserData> OnUserDataChanged;
+
+    public void SetUser(UserData userData)
+    {
+        CurrentUser = userData;
+    }
+
+    public void ClearUser()
+    {
+        CurrentUser = null;
+    }
+
+    public void UpdateUser(UserData updatedUserData)
+    {
+        if (_currentUser == null)
+        {
+            Debug.LogWarning("[UserManager] No current user");
+            return;
+        }
+
+        CurrentUser = updatedUserData;
+    }
 }
