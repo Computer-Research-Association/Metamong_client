2ba4cd1426f0542de1d65af68b225236a40ea8de
Jangmyun <cndlsrb2739@gmail.com>
2026-02-20 18:41:20 +0900

    [Feat]: FastAPI 모델 수정에 따라 Enum 프로퍼티 추가 및 UserResponse 양식 변경

--- Assets/Scripts/Core/ServerSchemas.cs
+++ Assets/Scripts/Core/ServerSchemas.cs
@@ -8,19 +8,28 @@ namespace Metamong.Core
   [JsonConverter(typeof(StringEnumConverter))]
   public enum AuthProvider
   {
-    GOOGLE, KAKAO, NAVER
+    GOOGLE, NAVER, KAKAO, LOCAL
   }
 
   [JsonConverter(typeof(StringEnumConverter))]
   public enum RC
   {
-    Torrey, JangGiRyeo, Kuyper, SonYangWon, Philadelphos, Carmichael
+    UNASSIGNED, Torrey, JangGiRyeo, Kuyper, SonYangWon, Philadelphos, Carmichael
   }
 
   [JsonConverter(typeof(StringEnumConverter))]
   public enum UserStatus
   {
-    ACTIVE, INACTIVE, BANNED
+    ACTIVE, SUSPENDED, DELETED, NEW, GUEST
+  }
+
+  [JsonConverter(typeof(StringEnumConverter))]
+  public enum MBTI    // 추가
+  {
+    ISTJ, ISFJ, INFJ, INTJ,
+    ISTP, ISFP, INFP, INTP,
+    ESTP, ESFP, ENFP, ENTP,
+    ESTJ, ESFJ, ENFJ, ENTJ
   }
 
   #endregion
@@ -29,19 +38,37 @@ namespace Metamong.Core
 
   public class UserData
   {
-    public int Id { get; set; }
-    public string Email { get; set; }
-    public string Nickname { get; set; }
+    [JsonProperty("id")] public int Id { get; set; }
+    [JsonProperty("email")] public string Email { get; set; }
+    [JsonProperty("nickname")] public string Nickname { get; set; }
+    [JsonProperty("real_name")] public string RealName { get; set; }
+    [JsonProperty("auth_provider")] public AuthProvider AuthProvider { get; set; }
+    [JsonProperty("rc")] public RC Rc { get; set; }
+    [JsonProperty("status")] public UserStatus Status { get; set; }
 
-    [JsonProperty("real_name")]
-    public string RealName { get; set; }
+    // 추가 프로필 필드
+    [JsonProperty("student_id")] public string StudentId { get; set; }
+    [JsonProperty("major")] public string Major { get; set; }
+    [JsonProperty("phone_number")] public string PhoneNumber { get; set; }
+    [JsonProperty("instagram_id")] public string InstagramId { get; set; }
+    [JsonProperty("mbti")] public MBTI? Mbti { get; set; }
 
-    public AuthProvider AuthProvider { get; set; }
-    public RC Rc { get; set; }
-    public UserStatus Status { get; set; }
+    // 위치 정보
+    [JsonProperty("last_room_id")] public int? LastRoomId { get; set; }
+    [JsonProperty("last_room_x")] public int? LastRoomX { get; set; }
+    [JsonProperty("last_room_y")] public int? LastRoomY { get; set; }
 
-    [JsonProperty("last_room_id")]
-    public int? LastRoomId { get; set; }
+    // 헬퍼 프로퍼티 ---------------------------------------------------------
+
+    /// <summary>최초 가입 후 프로필 미완성 상태</summary>
+    public bool IsNewUser => Status == UserStatus.NEW;
+
+    /// <summary>활동 가능한 상태인지 여부</summary>
+    public bool IsActive => Status == UserStatus.ACTIVE;
+
+    /// <summary>마지막 위치 정보가 있는지 여부</summary>
+    public bool HasLastPosition =>
+        LastRoomId.HasValue && LastRoomX.HasValue && LastRoomY.HasValue;
   }
 
   #endregion
