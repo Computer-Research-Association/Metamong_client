31da252f459e850ca5d34ffdf232e9ccd51c3d2c
ㅇㅅㅇ <jhy041020@handong.ac.kr>
2026-03-07 21:58:53 +0900

    [Feat]: LoginUI State Script 추가

---
+++ Assets/Scripts/UIStates/LoginUI.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class LoginUI : MonoBehaviour, IState<UIDocument>
+{
+    UIDocument _uIDocument;
+    VisualElement _root;
+    Button _enterButton;
+    public void Enter(UIDocument owner)
+    {
+        _uIDocument = owner;
+        _uIDocument.gameObject.SetActive(true);
+    }
+    void OnEnable()
+    {
+        Init();
+    }
+    void Init()
+    {
+        if (_uIDocument == null) _uIDocument = transform.gameObject.GetComponent<UIDocument>();
+        _root = _uIDocument.rootVisualElement;
+        _enterButton = _root.Query<Button>("EnterButton");
+        _enterButton.clicked += OnButtonClicked;
+
+    }
+    void OnButtonClicked()
+    {
+        Debug.Log($"Enter button clicked");
+    }
+    public void Exit()
+    {
+        if (!_uIDocument) return;
+        _uIDocument.gameObject.SetActive(false);
+    }
+
+    public void Update()
+    {
+    }
+}
