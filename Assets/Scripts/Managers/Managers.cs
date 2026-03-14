3f355ac25ac25fdbf9bf8381ac221eafe521bfc2
Jangmyun <cndlsrb2739@gmail.com>
2026-03-14 22:59:03 +0900

    [Fix]: AuthManager->FastAPIHandler로 바뀌었고, NetworkCore에서 관리

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -7,17 +7,15 @@ public class Managers : MonoBehaviour
 
     #region [Sub Managers]
 
-    AuthManager _auth = new AuthManager();
     ResourceManager _resource = new ResourceManager();
     UIManager _ui;
 
 
-    public static AuthManager Auth { get { return Instance._auth; } }
     public static ResourceManager Resource { get { return Instance._resource; } }
     public static UIManager UI { get { return Instance._ui; } }
 
     //원래는 필요한 매니저에 주입을 해주어야 함.
-    public static UserData_ userData_= new UserData_();
+    public static UserData_ userData_ = new UserData_();
 
     #endregion
 
@@ -26,7 +24,6 @@ public class Managers : MonoBehaviour
         // AuthManager 에 Unity 준비 완료 신호 전달
         // -> jslib 통해 index.html 의 onUnityReady() 호출 -> SendMessage로 토큰 들어옴
         NetworkCore.Instance.LoadData(userData_);
-        _auth.Init();
         _ui.Init();
 
         //테스트 코드
